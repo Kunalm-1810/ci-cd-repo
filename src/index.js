87ed0712f215da65589973cb6d41fb89d8d1b16b
kunal mane <kode.techm@gmail.com>
2026-04-06 20:33:21 +0530

    all changes are done again

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => <h1>Hello, World! jenkins CI/CD project with github webhook</h1>;
 
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => <h1>Hello, World!</h1>;
-
->>>>>>> a24782a463ddfef10ca3fe7a51ed6f2e7fd18a5f
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));git 
